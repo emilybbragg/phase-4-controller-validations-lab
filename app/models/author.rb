4028f4a2cdf64f00124f9dda4e6129dff34529a2
Emily <emilybribragg@gmail.com>
2022-09-02 11:34:14 -0400

    commit tree be8a4516928fd91f7beb9fb3faf672928712ed30

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+
+# The name cannot be blank
+# validates :name, presence: true
+validates :name, presence: true
+# The e-mail is unique
+validates :email, uniqueness: true
+
 end
